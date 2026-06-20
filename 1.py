9cf5bfb28b8151e2293dfd958b7dd451b75e0d82
hrdpaliwal93 <hardikpaliwal2015@gmail.com>
2026-06-20 19:42:04 +0530

    1

--- 1.py
+++ 1.py
@@ -2,6 +2,10 @@
 
 class Car:
     def __init__(self, brand, model):
+        # this init function is mainly used to establish a connection between the class and the called object of this class. it is a constructor used to initialize the object 
+
+        #the object has the access of itself using the SELF keyword
+        #self recceives the object that called the method, stores the value inside the specific object that called the method.
         self.brand = brand
         self.model = model
 car = Car("bmw", "m4")
