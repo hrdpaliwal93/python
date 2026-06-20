7fec0a8cfe7f7f0326809433d508bfc2cf33cb90
hrdpaliwal93 <hardikpaliwal2015@gmail.com>
2026-06-20 19:24:48 +0530

    getting started

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-print("hardik")
+# create a class CARS with attributes brand and model, create an instance of this class
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+car = Car("bmw", "m4")
+
+print("the model ",car.model,"belongs to the ",car.brand,"brand")
